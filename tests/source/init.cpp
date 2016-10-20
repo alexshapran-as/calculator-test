ae4ec1602d1f52de085286580117cde53334df35
Alex Shapran <shapran400@gmail.com>
2016-10-21 00:17:38 +0300

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -2,10 +2,50 @@
 #include <catch.hpp>
 
 SCENARIO("calculator sum", "[sum]") {
-    int x = 3;
-    int y = 4;
+    float x = 9.0;
+    float y = 1.0;
     
-    int rv = sum( x, y );
+    double rv = sum( x, y );
     
-	REQUIRE( rv == 7);
+	REQUIRE( rv == 10.0 );
+}
+SCENARIO("calculator sub", "[sub]") {
+    float x = 9.0;
+    float y = 1.0;
+    
+    double rv = sub( x, y );
+    
+	REQUIRE( rv == 8.0 );
+}
+SCENARIO("calculator mult", "[mult]") {
+    float x = 9.0;
+    float y = 1.0;
+    
+    double rv = mult( x, y );
+    
+	REQUIRE( rv == 9.0 );
+}
+SCENARIO("calculator div", "[div]") {
+    float x = 9.0;
+    float y = 1.0;
+    
+    double rv = div( x, y );
+    
+	REQUIRE( rv == 9.0 );
+}
+SCENARIO("calculator pow", "[pow]") {
+    float x = 9.0;
+    float y = 1.0;
+    
+    double rv = pow( x, y );
+    
+	REQUIRE( rv == 6561.0 );
+}
+SCENARIO("calculator sq", "[sq]") {
+    float x = 9.0;
+    float y = 1.0;
+    
+    double rv = sq( x, y );
+    
+	REQUIRE( rv == 3.0 );
 }
