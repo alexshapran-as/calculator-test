3761eb35d02c90fde360c9b9f9fa5a0f6d05b278
Alex Shapran <shapran400@gmail.com>
2016-10-28 12:20:39 +0300

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -2,50 +2,50 @@
 #include <catch.hpp>
 
 SCENARIO("calculator sum", "[sum]") {
-    float x = 9.0;
-    float y = 1.0;
+    float a = 9.0;
+    float b = 1.0;
     
-    double rv = sum( x, y );
+    double rv = sum( a, b );
     
 	REQUIRE( rv == 10.0 );
 }
 SCENARIO("calculator sub", "[sub]") {
-    float x = 9.0;
-    float y = 1.0;
+    float a = 9.0;
+    float b = 1.0;
     
-    double rv = sub( x, y );
+    double rv = sub( a, b );
     
 	REQUIRE( rv == 8.0 );
 }
 SCENARIO("calculator mult", "[mult]") {
-    float x = 9.0;
-    float y = 1.0;
+    float a = 9.0;
+    float b = 1.0;
     
-    double rv = mult( x, y );
+    double rv = mult( a, b );
     
 	REQUIRE( rv == 9.0 );
 }
 SCENARIO("calculator div", "[div]") {
-    float x = 9.0;
-    float y = 1.0;
+    float a = 9.0;
+    float b = 1.0;
     
-    double rv = div( x, y );
+    double rv = div( a, b );
     
 	REQUIRE( rv == 9.0 );
 }
 SCENARIO("calculator pow", "[pow]") {
-    float x = 9.0;
-    float y = 1.0;
+    float a = 9.0;
+    float b = 1.0;
     
-    double rv = pow( x );
+    double rv = pow( a );
     
 	REQUIRE( rv == 6561.0 );
 }
 SCENARIO("calculator sq", "[sq]") {
-    float x = 9.0;
-    float y = 1.0;
+    float a = 9.0;
+    float b = 1.0;
     
-    double rv = sq( x );
+    double rv = sq( a );
     
 	REQUIRE( rv == 3.0 );
 }
